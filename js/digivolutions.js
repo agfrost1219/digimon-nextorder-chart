e0f590a8ea6842663f362bd385a8ffc1fd088ed5
Andrew Frost <agfrost1219@gmail.com>
2017-02-05 22:14:19 -0500

    Split Data
    Split data into separate files for easier editing later.

---
+++ js/digivolutions.js
@@ -0,0 +1,1201 @@
+var DigimonEvolutions = [
+  {
+    "Name": "Koromon",
+    "Evolution": [
+      "Agumon",
+      "Agumon (Black)",
+      "ToyAgumon",
+      "Veemon",
+      "Shoutmon"
+    ]
+  },
+  {
+    "Name": "Tsunomon",
+    "Evolution": [
+      "Gabumon",
+      "Gaomon",
+      "Psychemon",
+      "Gabumon (Black)",
+      "Gumdramon"
+    ]
+  },
+  {
+    "Name": "Tokomon",
+    "Evolution": [
+      "Patamon",
+      "Biyomon",
+      "Tsukaimon",
+      "Hackmon",
+      "Lucemon"
+    ]
+  },
+  {
+    "Name": "Nyaromon",
+    "Evolution": [
+      "Salamon",
+      "SnowAgumon",
+      "Renamon",
+      "Biyomon",
+      "Lucemon"
+    ]
+  },
+  {
+    "Name": "Motimon",
+    "Evolution": [
+      "Tentomon",
+      "Wormmon",
+      "Gotsumon",
+      "Hackmon",
+      "Hagurumon"
+    ]
+  },
+  {
+    "Name": "Gigimon",
+    "Evolution": [
+      "Guilmon",
+      "Goburimon",
+      "Solarmon",
+      "Gotsumon",
+      "Shoutmon"
+    ]
+  },
+  {
+    "Name": "Gummymon",
+    "Evolution": [
+      "Terriermon",
+      "ToyAgumon",
+      "ClearAgumon",
+      "Hagurumon",
+      "Gaomon"
+    ]
+  },
+  {
+    "Name": "Kokomon",
+    "Evolution": [
+      "Lopmon",
+      "Demidevimon",
+      "ToyAgumon (Black)",
+      "Shamanmon",
+      "Wormmon"
+    ]
+  },
+  {
+    "Name": "Bukamon",
+    "Evolution": [
+      "Gomamon",
+      "Veemon",
+      "SnowGoburimon",
+      "ClearAgumon",
+      "Gumdramon"
+    ]
+  },
+  {
+    "Name": "Tanemon",
+    "Evolution": [
+      "Palmon",
+      "Renamon",
+      "Aruraumon",
+      "Goburimon",
+      "Demidevimon"
+    ]
+  },
+  {
+    "Name": "Tsumemon",
+    "Evolution": [
+      "Keramon",
+      "ToyAgumon (Black)",
+      "Agumon (Black)",
+      "Gabumon (Black)",
+      "Tsukaimon"
+    ]
+  },
+  {
+    "Name": "Agumon",
+    "Evolution": [
+      "Greymon",
+      "Tyrannomon",
+      "Meramon",
+      "Greymon (Blue)"
+    ]
+  },
+  {
+    "Name": "Gabumon",
+    "Evolution": [
+      "Garurumon",
+      "Gaogamon",
+      "Garurumon (Black)",
+      "Seadramon"
+    ]
+  },
+  {
+    "Name": "Biyomon",
+    "Evolution": [
+      "Birdramon",
+      "Piddomon",
+      "RedVeedramon",
+      "Youkomon"
+    ]
+  },
+  {
+    "Name": "Patamon",
+    "Evolution": [
+      "Angemon",
+      "Piddomon",
+      "Veedramon",
+      "Growlmon (Orange)"
+    ]
+  },
+  {
+    "Name": "Tentomon",
+    "Evolution": [
+      "Kabuterimon",
+      "RedVegiemon",
+      "Woodmon",
+      "BomberNanimon"
+    ]
+  },
+  {
+    "Name": "Gomamon",
+    "Evolution": [
+      "Ikkakumon",
+      "Seadramon",
+      "Icemon",
+      "Sorcermon"
+    ]
+  },
+  {
+    "Name": "Palmon",
+    "Evolution": [
+      "Togemon",
+      "Woodmon",
+      "Vegiemon",
+      "RedVegiemon"
+    ]
+  },
+  {
+    "Name": "Salamon",
+    "Evolution": [
+      "Gatomon",
+      "BlackGatomon",
+      "Gargomon",
+      "Meicoomon"
+    ]
+  },
+  {
+    "Name": "Veemon",
+    "Evolution": [
+      "ExVeemon",
+      "Veedramon",
+      "Meramon",
+      "GoldVeedramon"
+    ]
+  },
+  {
+    "Name": "Wormmon",
+    "Evolution": [
+      "Stingmon",
+      "Kuwagamon",
+      "Kabuterimon",
+      "Devimon"
+    ]
+  },
+  {
+    "Name": "Guilmon",
+    "Evolution": [
+      "Growlmon",
+      "Growlmon (Orange)",
+      "Tyrannomon",
+      "RedVeedramon"
+    ]
+  },
+  {
+    "Name": "Terriermon",
+    "Evolution": [
+      "Gargomon",
+      "Angemon",
+      "Ikkakumon",
+      "Ogremon"
+    ]
+  },
+  {
+    "Name": "Lopmon",
+    "Evolution": [
+      "Turuiemon",
+      "Kyuubimon (Silver)",
+      "Wizardmon",
+      "BlackGatomon"
+    ]
+  },
+  {
+    "Name": "Renamon",
+    "Evolution": [
+      "Kyuubimon",
+      "Kyuubimon (Silver)",
+      "Turuiemon",
+      "Leomon"
+    ]
+  },
+  {
+    "Name": "Gaomon",
+    "Evolution": [
+      "Gaogamon",
+      "Garurumon",
+      "Leomon",
+      "Togemon"
+    ]
+  },
+  {
+    "Name": "Hagurumon",
+    "Evolution": [
+      "Guardromon",
+      "GoldVeedramon",
+      "Kuwagamon",
+      "BomberNanimon"
+    ]
+  },
+  {
+    "Name": "Gotsumon",
+    "Evolution": [
+      "Icemon",
+      "Ogremon",
+      "Tyrannomon",
+      "Nanimon"
+    ]
+  },
+  {
+    "Name": "Goburimon",
+    "Evolution": [
+      "Ogremon",
+      "Nanimon",
+      "Woodmon",
+      "Gaogamon"
+    ]
+  },
+  {
+    "Name": "ToyAgumon",
+    "Evolution": [
+      "Wizardmon",
+      "Greymon",
+      "Guardromon (Gold)",
+      "Growlmon (Orange)"
+    ]
+  },
+  {
+    "Name": "Demidevimon",
+    "Evolution": [
+      "Devimon",
+      "BlackGatomon",
+      "IceDevimon"
+    ]
+  },
+  {
+    "Name": "Lucemon",
+    "Evolution": [
+      "IceDevimon",
+      "Piddomon",
+      "Wizardmon"
+    ]
+  },
+  {
+    "Name": "Hackmon",
+    "Evolution": [
+      "Leomon",
+      "Gururumon",
+      "ExVeemon",
+      "GoldVeedramon"
+    ]
+  },
+  {
+    "Name": "Agumon (Black)",
+    "Evolution": [
+      "Greymon (Blue)",
+      "Growlmon",
+      "Saberdramon",
+      "Fugamon"
+    ]
+  },
+  {
+    "Name": "SnowAgumon",
+    "Evolution": [
+      "Sorcermon",
+      "Gatomon",
+      "Hyogamon",
+      "Icemon"
+    ]
+  },
+  {
+    "Name": "Gabumon (Black)",
+    "Evolution": [
+      "Garurumon (Black)",
+      "Stingmon",
+      "Gururumon",
+      "Saberdramon"
+    ]
+  },
+  {
+    "Name": "Psychemon",
+    "Evolution": [
+      "Gururumon",
+      "Kyuubimon",
+      "BomberNanimon",
+      "Gargomon"
+    ]
+  },
+  {
+    "Name": "Tsukaimon",
+    "Evolution": [
+      "Saberdramon",
+      "Devimon",
+      "Youkomon",
+      "Garurumon (Black)"
+    ]
+  },
+  {
+    "Name": "Aruraumon",
+    "Evolution": [
+      "Vegiemon",
+      "Youkomon",
+      "Togemon",
+      "Stingmon"
+    ]
+  },
+  {
+    "Name": "ToyAgumon (Black)",
+    "Evolution": [
+      "Nanimon",
+      "Guardromon",
+      "Fugamon",
+      "Greymon (Blue)"
+    ]
+  },
+  {
+    "Name": "ClearAgumon",
+    "Evolution": [
+      "Guardromon (Gold)",
+      "IceDevimon",
+      "ExVeemon",
+      "Hyogamon"
+    ]
+  },
+  {
+    "Name": "Solarmon",
+    "Evolution": [
+      "Meramon",
+      "RedVegiemon",
+      "Birdramon",
+      "Guardromon (Gold)"
+    ]
+  },
+  {
+    "Name": "SnowGoburimon",
+    "Evolution": [
+      "Hyogamon",
+      "Ikkakumon",
+      "Seadramon"
+    ]
+  },
+  {
+    "Name": "Shamanmon",
+    "Evolution": [
+      "Fugamon",
+      "Vegiemon",
+      "Kuwagamon",
+      "Sorcermon"
+    ]
+  },
+  {
+    "Name": "Keramon",
+    "Evolution": [
+      "Chrysalimon",
+      "Guardromon",
+      "Meicoomon"
+    ]
+  },
+  {
+    "Name": "Gumdramon",
+    "Evolution": [
+      "Arresterdramon",
+      "Veedramon",
+      "Turuiemon",
+      "Gatomon"
+    ]
+  },
+  {
+    "Name": "Shoutmon",
+    "Evolution": [
+      "OmniShoutmon",
+      "Growlmon",
+      "Greymon",
+      "RedVeedramon"
+    ]
+  },
+  {
+    "Name": "Greymon",
+    "Evolution": [
+      "MetalGreymon",
+      "MetalGreymon (Blue)",
+      "WarGrowlmon (Orange)"
+    ]
+  },
+  {
+    "Name": "Garurumon",
+    "Evolution": [
+      "WereGarurumon",
+      "GrapLeomon",
+      "MachGaogamon"
+    ]
+  },
+  {
+    "Name": "Birdramon",
+    "Evolution": [
+      "Garudamon",
+      "AeroVeedramon",
+      "Agunimon"
+    ]
+  },
+  {
+    "Name": "Angemon",
+    "Evolution": [
+      "MagnaAngemon",
+      "BlueMeramon",
+      "Taomon"
+    ]
+  },
+  {
+    "Name": "Kabuterimon",
+    "Evolution": [
+      "MegaKabuterimon",
+      "MegaKabuterimon (Blue)",
+      "SkullGreymon"
+    ]
+  },
+  {
+    "Name": "Ikkakumon",
+    "Evolution": [
+      "Zudomon",
+      "IceLeomon",
+      "Rapidmon"
+    ]
+  },
+  {
+    "Name": "Togemon",
+    "Evolution": [
+      "Lillymon",
+      "Mamemon",
+      "LadyDevimon"
+    ]
+  },
+  {
+    "Name": "Gatomon",
+    "Evolution": [
+      "Angewomon",
+      "Taomon (Silver)",
+      "Lillymon",
+      "Angewomon",
+      "Meteormon",
+      "Lillymon"
+    ]
+  },
+  {
+    "Name": "ExVeemon",
+    "Evolution": [
+      "AeroVeedramon",
+      "MetalTyrannomon",
+      "Garudamon"
+    ]
+  },
+  {
+    "Name": "Stingmon",
+    "Evolution": [
+      "MegaKabuterimon (Blue)",
+      "Okuwamon",
+      "MegaKabuterimon"
+    ]
+  },
+  {
+    "Name": "Growlmon",
+    "Evolution": [
+      "WarGrowlmon",
+      "SkullGreymon",
+      "BlackWarGrowlmon"
+    ]
+  },
+  {
+    "Name": "Gargomon",
+    "Evolution": [
+      "Rapidmon",
+      "MetalMamemon",
+      "Rapidmon (Gold)"
+    ]
+  },
+  {
+    "Name": "Turuiemon",
+    "Evolution": [
+      "Antylamon",
+      "Megadramon",
+      "Monzaemon"
+    ]
+  },
+  {
+    "Name": "Kyuubimon",
+    "Evolution": [
+      "Taomon",
+      "Monzaemon",
+      "WereGarurumon"
+    ]
+  },
+  {
+    "Name": "Gaogamon",
+    "Evolution": [
+      "MachGaogamon",
+      "WereGarurumon",
+      "GrapLeomon"
+    ]
+  },
+  {
+    "Name": "Wizardmon",
+    "Evolution": [
+      "Lucemon FM",
+      "Myotismon",
+      "MagnaAngemon"
+    ]
+  },
+  {
+    "Name": "Devimon",
+    "Evolution": [
+      "Myotismon",
+      "Doumon",
+      "Antylamon"
+    ]
+  },
+  {
+    "Name": "Veedramon",
+    "Evolution": [
+      "AeroVeedramon",
+      "MachGaogamon",
+      "MegaSeadramon"
+    ]
+  },
+  {
+    "Name": "Tyrannomon",
+    "Evolution": [
+      "MetalTyrannomon",
+      "MetalGreymon (Blue)",
+      "WarGrowlmon"
+    ]
+  },
+  {
+    "Name": "Ogremon",
+    "Evolution": [
+      "SkullGreymon",
+      "WaruSeadramon",
+      "MetalTyrannomon"
+    ]
+  },
+  {
+    "Name": "Leomon",
+    "Evolution": [
+      "GrapLeomon",
+      "Rapidmon",
+      "IceLeomon"
+    ]
+  },
+  {
+    "Name": "Meramon",
+    "Evolution": [
+      "BlueMeramon",
+      "Garudamon",
+      "Myotismon"
+    ]
+  },
+  {
+    "Name": "Vegiemon",
+    "Evolution": [
+      "Mamemon",
+      "Etemon",
+      "Okuwamon"
+    ]
+  },
+  {
+    "Name": "Nanimon",
+    "Evolution": [
+      "WaruSeadramon",
+      "Doumon",
+      "Etemon"
+    ]
+  },
+  {
+    "Name": "Kuwagamon",
+    "Evolution": [
+      "Okuwamon",
+      "Taomon",
+      "Mamemon"
+    ]
+  },
+  {
+    "Name": "Seadramon",
+    "Evolution": [
+      "Gigadramon",
+      "MegaSeadramon",
+      "WaruSeadramon"
+    ]
+  },
+  {
+    "Name": "Guardromon",
+    "Evolution": [
+      "Datamon",
+      "MetalMamemon",
+      "MetalTyrannomon"
+    ]
+  },
+  {
+    "Name": "Woodmon",
+    "Evolution": [
+      "Okuwamon",
+      "Taomon",
+      "Mamemon"
+    ]
+  },
+  {
+    "Name": "BomberNanimon",
+    "Evolution": [
+      "Megadramon",
+      "Meteormon",
+      "MetalMamemon"
+    ]
+  },
+  {
+    "Name": "Icemon",
+    "Evolution": [
+      "Mamemon",
+      "Meteormon",
+      "Zudomon"
+    ]
+  },
+  {
+    "Name": "Hyogamon",
+    "Evolution": [
+      "Zudomon",
+      "BlueMeramon",
+      ""
+    ]
+  },
+  {
+    "Name": "Piddomon",
+    "Evolution": [
+      "Meteormon",
+      "Angewomon",
+      "Monzaemon"
+    ]
+  },
+  {
+    "Name": "Kyuubimon (Silver)",
+    "Evolution": [
+      "Taomon (Silver)",
+      "Gigadramon",
+      "Antylamon"
+    ]
+  },
+  {
+    "Name": "Gururumon",
+    "Evolution": [
+      "BlackWarGrowlmon",
+      "WereGarurumon (Black)",
+      "Taomon (Silver)"
+    ]
+  },
+  {
+    "Name": "Saberdramon",
+    "Evolution": [
+      "Megadramon",
+      "WaruSeadramon",
+      "LadyDevimon"
+    ]
+  },
+  {
+    "Name": "BlackGatomon",
+    "Evolution": [
+      "LadyDevimon",
+      "Antylamon"
+    ]
+  },
+  {
+    "Name": "Fugamon",
+    "Evolution": [
+      "BlackWarGrowlmon",
+      "WereGarurumon (Black)",
+      "MachGaogamon"
+    ]
+  },
+  {
+    "Name": "Guardromon (Gold)",
+    "Evolution": [
+      "MetalMamemon",
+      "Datamon"
+    ]
+  },
+  {
+    "Name": "Youkomon",
+    "Evolution": [
+      "Doumon",
+      "MegaKabuterimon (Blue)",
+      "WereGarurumon (Black)"
+    ]
+  },
+  {
+    "Name": "RedVeedramon",
+    "Evolution": [
+      "WarGrowlmon",
+      "MegaKabuterimon",
+      "Agunimon"
+    ]
+  },
+  {
+    "Name": "GoldVeedramon",
+    "Evolution": [
+      "MegaSeadramon",
+      "MetalGreymon",
+      "Rapidmon (Gold)"
+    ]
+  },
+  {
+    "Name": "Growlmon (Orange)",
+    "Evolution": [
+      "WarGrowlmon (Orange)",
+      "MetalGreymon",
+      "BlackWarGrowlmon"
+    ]
+  },
+  {
+    "Name": "Greymon (Blue)",
+    "Evolution": [
+      "MetalGreymon (Blue)",
+      "SkullGreymon"
+    ]
+  },
+  {
+    "Name": "Garurumon (Black)",
+    "Evolution": [
+      "WereGarurumon (Black)",
+      "Doumon",
+      "Etemon"
+    ]
+  },
+  {
+    "Name": "RedVegiemon",
+    "Evolution": [
+      "Lillymon",
+      "WarGrowlmon (Orange)",
+      "MegaKabuterimon"
+    ]
+  },
+  {
+    "Name": "IceDevimon",
+    "Evolution": [
+      "Gigadramon",
+      "Zudomon",
+      "Taomon (Silver)"
+    ]
+  },
+  {
+    "Name": "Sorcerymon",
+    "Evolution": [
+      "IceLeomon",
+      "MagnaAngemon",
+      "BlueMeramon"
+    ]
+  },
+  {
+    "Name": "Chrysalimon",
+    "Evolution": [
+      "Infermon",
+      "Datamon",
+      ""
+    ]
+  },
+  {
+    "Name": "Numemon",
+    "Evolution": "Monzaemon"
+  },
+  {
+    "Name": "Geremon",
+    "Evolution": "Etemon"
+  },
+  {
+    "Name": "Meicoomon",
+    "Evolution": [
+      "LadyDevimon",
+      "Angewomon"
+    ]
+  },
+  {
+    "Name": "MetalGreymon",
+    "Evolution": [
+      "WarGreymon",
+      "ShineGreymon",
+      "KaiserGreymon",
+      "Alphamon"
+    ]
+  },
+  {
+    "Name": "WereGarurumon",
+    "Evolution": [
+      "MetalGarurumon",
+      "Jijimon",
+      "Kentaurosmon"
+    ]
+  },
+  {
+    "Name": "Garudamon",
+    "Evolution": [
+      "Phoenixmon",
+      "Magnadramon",
+      "Beelzemon",
+      "Justimon"
+    ]
+  },
+  {
+    "Name": "MagnaAngemon",
+    "Evolution": [
+      "Seraphimon",
+      "Cherubimon",
+      "Jesmon",
+      "MarineAngemon"
+    ]
+  },
+  {
+    "Name": "MegaKabuterimon",
+    "Evolution": [
+      "HerculesKabuterimon",
+      "ChaosGallantmon",
+      "Craniamon"
+    ]
+  },
+  {
+    "Name": "Zudomon",
+    "Evolution": [
+      "Vikemon",
+      "MetalSeadramon",
+      "Titamon",
+      "Boltmon"
+    ]
+  },
+  {
+    "Name": "Lillymon",
+    "Evolution": [
+      "Rosemon",
+      "Sakuyamon",
+      "Lilithmon",
+      "Minervamon"
+    ]
+  },
+  {
+    "Name": "Angewomon",
+    "Evolution": [
+      "Magnadramon",
+      "Ophanimon",
+      "Crusadermon",
+      "MarineAngemon"
+    ]
+  },
+  {
+    "Name": "Paildramon",
+    "Evolution": [
+      "Magnamon",
+      "Imperialdramon DM",
+      "UlforceVeedramon"
+    ]
+  },
+  {
+    "Name": "WarGrowlmon",
+    "Evolution": [
+      "Gallantmon",
+      "ShineGreymon",
+      "Leviamon",
+      "KaiserGreymon"
+    ]
+  },
+  {
+    "Name": "Rapidmon",
+    "Evolution": [
+      "MegaGargomon",
+      "Vikemon",
+      "Kentaurosmon",
+      "MagnaGarurumon"
+    ]
+  },
+  {
+    "Name": "Antylamon",
+    "Evolution": [
+      "Cherubimon",
+      "Ophanimon",
+      "Belphemon SM"
+    ]
+  },
+  {
+    "Name": "Taomon",
+    "Evolution": [
+      "Sakuyamon",
+      "Dianamon",
+      "Kuzuhamon",
+      "Barbamon"
+    ]
+  },
+  {
+    "Name": "MachGaogamon",
+    "Evolution": [
+      "MirageGaogamon",
+      "MetalGarurumon",
+      "Gankoomon",
+      "Justimon"
+    ]
+  },
+  {
+    "Name": "AeroVeedramon",
+    "Evolution": [
+      "UlforceVeedramon",
+      "OmniShoutmon",
+      "Jesmon",
+      "DarkDramon"
+    ]
+  },
+  {
+    "Name": "Myotismon",
+    "Evolution": [
+      "VenomMyotismon",
+      "Jijimon",
+      "Barbamon"
+    ]
+  },
+  {
+    "Name": "LadyDevimon",
+    "Evolution": [
+      "Lilithmon",
+      "Rosemon",
+      "Kuzuhamon",
+      "Piedmon"
+    ]
+  },
+  {
+    "Name": "Mamemon",
+    "Evolution": [
+      "Kentaurosmon",
+      "PlatinumNumemon",
+      "Justimon",
+      "Jijimon"
+    ]
+  },
+  {
+    "Name": "MetalMamemon",
+    "Evolution": [
+      "Dynasmon",
+      "Machinedramon",
+      "MetalGarurumon",
+      "MagnaGarurumon"
+    ]
+  },
+  {
+    "Name": "MetalTyrannomon",
+    "Evolution": [
+      "WarGreymon",
+      "Titamon",
+      "RustTyrannomon"
+    ]
+  },
+  {
+    "Name": "SkullGreymon",
+    "Evolution": [
+      "Craniamon",
+      "Titamon",
+      "BlackWarGreymon"
+    ]
+  },
+  {
+    "Name": "Gigadramon",
+    "Evolution": [
+      "RustTyrannomon",
+      "Machinedramon",
+      "Justimon",
+      "Dynasmon"
+    ]
+  },
+  {
+    "Name": "Megadramon",
+    "Evolution": [
+      "Gaioumon",
+      "WarGreymon",
+      "DarkDramon",
+      "Belphemon SM",
+      ""
+    ]
+  },
+  {
+    "Name": "IceLeomon",
+    "Evolution": [
+      "Gankoomon",
+      "Magnadramon"
+    ]
+  },
+  {
+    "Name": "GrapLeomon",
+    "Evolution": [
+      "BanchoLeomon",
+      "MegaGargomon",
+      "Dynasmon"
+    ]
+  },
+  {
+    "Name": "BlueMeramon",
+    "Evolution": [
+      "Leviamon",
+      "MirageGaogamon",
+      "Boltmon",
+      "Gankoomon"
+    ]
+  },
+  {
+    "Name": "MegaSeadramon",
+    "Evolution": [
+      "MetalSeadramon",
+      "Machinedramon"
+    ]
+  },
+  {
+    "Name": "Okuwamon",
+    "Evolution": [
+      "GranKuwagamon",
+      "Examon",
+      "Diaboromon"
+    ]
+  },
+  {
+    "Name": "Datamon",
+    "Evolution": [
+      "Barbamon",
+      "PlatinumNumemon",
+      "MagnaGarurumon"
+    ]
+  },
+  {
+    "Name": "Meteormon",
+    "Evolution": [
+      "Belphemon SM",
+      "Dianamon",
+      "Cherubimon"
+    ]
+  },
+  {
+    "Name": "MetalGreymon (Blue)",
+    "Evolution": [
+      "BlackWarGreymon",
+      "Gaioumon",
+      "Alphamon",
+      "Boltmon"
+    ]
+  },
+  {
+    "Name": "WereGarurumon (Black)",
+    "Evolution": [
+      "MetalGarurumon (Black)",
+      "Duftmon",
+      "Minervamon",
+      "MirageGaogamon"
+    ]
+  },
+  {
+    "Name": "MegaKabuterimon (Blue)",
+    "Evolution": [
+      "Craniamon",
+      "GranKuwagamon",
+      "Kentaurosmon"
+    ]
+  },
+  {
+    "Name": "WarGrowlmon (Orange)",
+    "Evolution": [
+      "Examon",
+      "RustTyrannomon",
+      "WarGreymon"
+    ]
+  },
+  {
+    "Name": "BlackWarGrowlmon",
+    "Evolution": [
+      "ChaosGallantmon",
+      "Beelzemon"
+    ]
+  },
+  {
+    "Name": "Rapidmon (Gold)",
+    "Evolution": [
+      "Duftmon",
+      "HerculesKabuterimon"
+    ]
+  },
+  {
+    "Name": "Taomon (Silver)",
+    "Evolution": [
+      "Crusadermon",
+      "Phoenixmon",
+      "Dianamon",
+      ""
+    ]
+  },
+  {
+    "Name": "Doumon",
+    "Evolution": [
+      "Kuzuhamon",
+      "MetalGarurumon (Black)",
+      "Lilithmon",
+      "Piedmon"
+    ]
+  },
+  {
+    "Name": "WaruSeadramon",
+    "Evolution": [
+      "Daemon",
+      "MetalSeadramon"
+    ]
+  },
+  {
+    "Name": "Infermon",
+    "Evolution": [
+      "Diaboromon",
+      "Daemon",
+      "VenomMyotismon"
+    ]
+  },
+  {
+    "Name": "Etemon",
+    "Evolution": [
+      "MetalEtemon",
+      "MegaGargomon",
+      "PlatinumNumemon"
+    ]
+  },
+  {
+    "Name": "Monzaemon",
+    "Evolution": [
+      "PlatinumNumemon",
+      "Seraphimon",
+      "Minervamon",
+      "Vikemon"
+    ]
+  },
+  {
+    "Name": "Agunimon",
+    "Evolution": [
+      "KaiserGreymon",
+      "Gallantmon",
+      "Phoenixmon",
+      "ShineGreymon"
+    ]
+  },
+  {
+    "Name": "Lucemon FM",
+    "Evolution": [
+      "VenomMyotismon",
+      "Lucemon SM"
+    ]
+  }
+]
